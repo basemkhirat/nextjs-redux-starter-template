63bda8016c104d53305445f731411130f52e56cc
basemkhirat <basemkhirat@gmail.com>
2020-05-05 14:35:55 +0200

    add partials and components folders

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ee8da24823448bdc1738";
+/******/ 	var hotCurrentHash = "d1cf863c296b4346dc6f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
